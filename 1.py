d3002442ea2fab18fe5ab07625004b4a0c7fe1ff
Mamta poudel <m.yrx.mamata@gmail.com>
2026-02-27 21:53:07 +0545

    nthg at all

---
+++ 1.py
@@ -0,0 +1,7 @@
+#calc area of rec
+#Example 1
+length=input("Enter the length of the rectangle:")
+breadth=int(input("Enter the breadth of the rectangle:"))
+area=int(length)*breadth
+print(f"The area of the rectangle is {area}")
+#Example 2
