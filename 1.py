61738692933f2a0dbb74b7d6ea7b05bc8c2c2560
Mamta poudel <m.yrx.mamata@gmail.com>
2026-02-27 22:31:09 +0545

    nthg*2

--- 1.py
+++ 1.py
@@ -4,4 +4,10 @@ length=input("Enter the length of the rectangle:")
 breadth=int(input("Enter the breadth of the rectangle:"))
 area=int(length)*breadth
 print(f"The area of the rectangle is {area}")
-#Example 2
+#Example 2 Shopping List
+item=input("Enter the item you want to purchase:")
+quantity=int(input("Enter the quantity of the item:"))
+price=float(input("Enter the price of the item:"))
+total_cost=quantity*price
+total_cost=float(total_cost)
+print(f"The total cost of {quantity} {item}(s) is {total_cost}")
