01d241c63611cf94e9a958465157c05846a4b844
Mamta poudel <m.yrx.mamata@gmail.com>
2026-05-28 09:53:57 +0545

    MULTIDIMENSIONAL LIST

---
+++ class3.py
@@ -0,0 +1,30 @@
+# emp=['Milan',102,'Nepal']
+# Dep1=['CS',10]
+# Dep2=['IT',11]
+# HOD_CS=[10,'Mr.CS']
+# HOD_IT=[11,'Mr.IT']
+# print('printing employee data.....')
+# print('Name:%s,ID:%d,Country:%s' % (emp[0], emp[1], emp[2]))
+# print('printing department data.....')
+# print('Department 1:\nName:%s,ID:%d\n Department 2:\nName:%s,ID:%d' % (Dep1[0], Dep1[1], Dep2[0], Dep2[1]))
+# print('printing HOD data.....')
+# print('CS HOD Name:%s,Id:%d' % (HOD_CS[1], HOD_CS[0]))
+# print('IT HOD Name:%s,Id:%d' % (HOD_IT[1], HOD_IT[0]))
+# print(type(emp),type(Dep1),type(Dep2),type(HOD_CS),type(HOD_IT))
+# print('printing employee data.....')
+# print('Name:%s,ID:%d,Country:%s' % (emp[0], emp[1], emp[2]))
+# print('Name:{}'.format(emp[0]))
+#multidimensional list
+# List=[['Welcome','to'],['Python','Class']]
+# print('Accessing a element from multidimensional list.....')
+# print(List[0])
+# print(List[0][0])
+# print(List[1][1])
+List=[[[['Wel'],['come']],['to']],['Python','Class']]
+print('Accessing a element from multidimensional list.....')
+print(List[0])
+print(List[0][0])
+print(List[0][1])
+print(List[0][0][0])
+print(List[0][0][1])
+print(List[1][1])
